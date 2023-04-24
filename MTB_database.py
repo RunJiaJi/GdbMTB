04289091828725602ed9ebb0d66f900cdb60104d
JianJiang <jianjiang.bio@gmail.com>
2023-04-24 14:10:22 +0800

    Add functions for uploading file

--- MTB_database.py
+++ MTB_database.py
@@ -1,6 +1,44 @@
-from flask import Flask, render_template, url_for
-app = Flask(__name__)
+from flask import Flask, render_template, url_for, request
+from datetime import datetime
+import os
 
+app = Flask(__name__)
+UPLOAD_FOLDER = 'user_files/'
+
+
+############################# functions ↓ ###########################
+def save_user_file(email, file):
+    new_path = os.path.join(UPLOAD_FOLDER, f'{datetime.utcnow().timestamp()}_{email}')
+    file.save(new_path)
+    return new_path
+
+def send_mail():
+    import smtplib
+    from email.mime.text import MIMEText
+
+    FROM='sender@qq.com'                                #发送方邮箱
+    PASSWD='bbphmxqqphpmidcj'                           #填入发送方邮箱的授权码
+    TO='receiver@gmail.com'                             #收件人邮箱
+    SUBJECT='TITLE'                                     #主题     
+    msg = MIMEText('CONTENT')
+
+    msg['Subject'] = SUBJECT
+    msg['From'] = FROM
+    msg['To'] = TO
+    try:
+        s = smtplib.SMTP_SSL("smtp.qq.com", 465)
+        s.login(FROM, PASSWD)
+        s.sendmail(FROM, TO, msg.as_string())
+        print("发送成功")
+    except:
+        print("发送失败")
+    finally:
+        s.quit()
+
+def handle_user_file(email, path):
+    pass
+
+#############################  routers  ↓ ###########################
 @app.route("/")
 @app.route("/home")
 def home():
@@ -27,7 +65,13 @@ def tools():
     if request.method == 'GET':
         return render_template('tools.html', title='Tools')
     if request.method == 'POST':
-        return 'Your sequence has been uploaded'
-    
+        print(request.form)
+        email = request.form['useremail']
+        file = request.files['userfile']
+        new_path = save_user_file(email, file)
+        handle_user_file(email, new_path)
+        return f'Your sequence has been uploaded successfully! An email will be sent to {email}, check later please.'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
