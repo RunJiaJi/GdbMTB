2748300c13bb7cda45c6b8aa775ae676591bd9da
RunJiaJi <jirunjia@gmail.com>
2023-08-26 01:25:31 +0800

    update

--- BrowserTableGenerator.py
+++ BrowserTableGenerator.py
@@ -4,7 +4,7 @@ def BrowserTableGenerator(meta_df):
     colClassi = ['Assembly accession','Name','NCBI taxonomy', 'GTDB taxonomy (r214)', 'External link 1','External link 2',]
     colRefere = ['Assembly accession','Name', 'Biosample accession', 'Bioproject accession', 'WGS accession', 'Sequencing platform', 'Submission date',
                 'Submitter','Title','PMID', 'DOI','Publish_date','Author',]
-    colEnv = ['Assembly accession','Name','Geographic location', 'Environment', 'Geographic coordinates', 'point','Latitude', 'longitude',]
+    colEnv = ['Assembly accession','Name','Geographic location', 'Environment']
     
     colTables=[colFea, colClassi, colRefere, colEnv]
     tablTables=[]
@@ -12,7 +12,9 @@ def BrowserTableGenerator(meta_df):
         dftmp=meta_df.loc[:,i]
         tablTable=dftmp.to_html(index=None, escape=False).replace(
             '<table border="1" class="dataframe">',
-            '<table class="table table-sm table-striped table-hover table table-fluid border" id="myTable">'
-        ).replace('<thead>','<thead class="table-dark align-middle">')
+            '<table class="table table-sm table-striped table-hover table table-fluid" id="myTable">'
+            ).replace(
+            '<thead>','<thead class="table-dark align-middle">'
+            )
         tablTables.append(tablTable)
     return tablTables
--- GdbMTB.py
+++ GdbMTB.py
@@ -3,16 +3,21 @@ import pandas as pd
 from BrowserTableGenerator import BrowserTableGenerator
 app = Flask(__name__)
 
-meta_df=pd.read_csv('./data/MTB_genomes_metadata_202307.tsv', sep='\t')
+meta_df=pd.read_csv('./data/MTB_genomes_metadata_202308.tsv', sep='\t')
 tablTables = BrowserTableGenerator(meta_df)
 
 with open('data/Tree_of_MTB.svg')as f:
     treeSVG=f.read()
 
+cities="""[{title: 'Sweden: Oskarshamn\nUncultured_microorganism_SbSrfc_SA12_01_D19', latitude: 57.4354483, longitude: 16.6691272},
+ {title: 'USA: Nevada\nOmnitrophica_bacterium_SCGC_AG-290-C17', latitude: 37.1312714, longitude: -116.8425033},
+ {title: 'Greece: Etoliko Lagoon\nRhodospirillaceae_bacterium_MAG_01419_mvb_30', latitude: 38.4884872, longitude: 21.2875175},
+ {title: 'USA: Sakinaw Lake\nLatescibacteria_bacterium_SCGC_AAA252-B13', latitude: 49.95898915, longitude: -123.8279332},]"""
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', cities=cities)
 
 @app.route("/browser/QualityandFeature")
 def browser_fea():
@@ -30,21 +35,29 @@ def browser_refre():
 def browser_env():
     return render_template('browser.html', title='Browser', table=tablTables[3], tableHeader='Environmental metadata')
 
-@app.route("/tree")
-def tree():
-    return render_template('tree.html', title='Tree', treeSVG=treeSVG)
+@app.route("/browser/MagnetosomeGeneClusters")
+def browser_mgc():
+    return render_template('browser-mgc.html', title='Browser')
+
+@app.route("/tree-taxa")
+def tree_taxa():
+    return render_template('tree-taxa.html', title='Tree', treeSVG=treeSVG)
+
+@app.route("/tree-tree")
+def tree_tree():
+    return render_template('tree-tree.html', title='Tree', treeSVG=treeSVG)
 
 @app.route("/statistics")
 def statistics():
     return render_template('statistics.html', title='Statistics')
 
-@app.route("/download")
+@app.route("/downloads")
 def download():
-    return render_template('download.html', title='Download')
+    return render_template('downloads.html', title='Download')
 
-@app.route("/tools")
+@app.route("/about")
 def tools():
-    return render_template('tools.html', title='Tools')
+    return render_template('about.html', title='Tools')
     
 if __name__ == '__main__':
     app.run(debug=True)
